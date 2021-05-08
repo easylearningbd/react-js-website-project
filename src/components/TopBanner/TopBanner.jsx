eb90dbe9c62affe6ac5d1329329043c3b8032839
easylearningbd <elebdsoft@gmail.com>
2021-05-09 04:47:46 +0600

    9 Top banner Component Part 2

--- src/components/TopBanner/TopBanner.jsx
+++ src/components/TopBanner/TopBanner.jsx
@@ -1,15 +1,25 @@
 import React, { Component, Fragment } from 'react'
-import { Container } from 'react-bootstrap'
+import { Col, Container, Row, Button } from 'react-bootstrap'
 import '../../asset/css/custom.css';
+import '../../asset/css/bootstrap.min.css';
 
 class TopBanner extends Component {
      render() {
           return (
                <Fragment>
-                    <Container className="topFixedBanner" >
-
-
-
+        <Container fluid={true} className="topFixedBanner p-0" >
+               <div className="topBannerOverlay">
+               <Container className="topContent">
+                    <Row>
+               <Col className="text-center">
+               <h1>EASY LEARNING</h1>
+               <h4>Learn Profesionally</h4>
+               <Button variant="primary">Learn More</Button>
+               
+               </Col>
+                    </Row> 
+               </Container> 
+               </div>
                     </Container>
                     
                </Fragment>
