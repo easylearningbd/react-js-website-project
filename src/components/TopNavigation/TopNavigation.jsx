1df073fb1c0dbb66c6de00ebd8317ee105e213d0
easylearningbd <elebdsoft@gmail.com>
2021-05-10 12:17:22 +0600

    15 Nav Component Part 2

--- src/components/TopNavigation/TopNavigation.jsx
+++ src/components/TopNavigation/TopNavigation.jsx
@@ -2,12 +2,33 @@ import React, { Component, Fragment } from 'react'
 import { Navbar,Nav,NavDropdown } from 'react-bootstrap'
 
 class TopNavigation extends Component {
+
+     constructor(){
+          super();
+          this.state={
+               navBarTitle:"navTitle"
+          }
+     }
+
+     onScroll=()=>{
+          if(window.scrollY>100){
+               this.setState({navBarTitle:'navTitleScroll'})
+          }else if(window.scrollY<100){
+               this.setState({navBarTitle:'navTitle'})
+          }
+     }
+
+          componentDidMount(){
+               window.addEventListener('scroll',this.onScroll)
+          }
+
+
      render() {
           return (
                 <Fragment>
   
       <Navbar collapseOnSelect fixed="top" expand="lg" bg="dark" variant="dark">
-  <Navbar.Brand href="#home">EASY LEARNING</Navbar.Brand>
+  <Navbar.Brand className={this.state.navBarTitle} href="#home">EASY LEARNING</Navbar.Brand>
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
   <Navbar.Collapse id="responsive-navbar-nav">
     <Nav className="mr-auto">
