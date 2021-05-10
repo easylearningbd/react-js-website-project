afe89661d3d8a7486e0b5020dd04ecded9b68b93
easylearningbd <elebdsoft@gmail.com>
2021-05-10 14:55:29 +0600

    22 Chart Component Part 3

--- src/components/Analysis/Analysis.jsx
+++ src/components/Analysis/Analysis.jsx
@@ -1,20 +1,57 @@
 import React, { Component, Fragment } from 'react'
 import { Col, Container, Row } from 'react-bootstrap'
+import { Bar, BarChart, ResponsiveContainer, Tooltip, XAxis } from 'recharts';
 
 class Analysis extends Component {
+
+     constructor(){
+          super();
+          this.state={
+               data:[
+                    {Techonology:'PHP',Projects:100},
+                    {Techonology:'MySqli',Projects:90},
+                    {Techonology:'Laravel',Projects:95},
+                    {Techonology:'React',Projects:85},
+                    {Techonology:'Opencart',Projects:80},
+                    {Techonology:'Vue Js',Projects:70},
+                    {Techonology:'Django',Projects:60},
+                    {Techonology:'JavaScript',Projects:100}                   
+               ]
+          }
+     }
+
+
+
      render() {
+          var blue = "#051b35"
           return (
                 <Fragment>
      <Container className="text-center">
      <h1 className="serviceMainTitle">TECHNOLOGY USED</h1>
                <div className="bottom"></div>
           <Row>
-               <Col lg={6} md={12} sm={12}>
-               <h1>BarChart</h1>
-               </Col>
+           <Col style={{width:'100%', height:'300px'}} lg={6} md={12} sm={12}>
+                 <ResponsiveContainer>   
+               <BarChart width={100} height={300} data={this.state.data}>
+
+               <XAxis dataKey="Techonology" /> 
+               <Tooltip />
+               <Bar dataKey="Projects" fill={blue}>
+
+               </Bar>
+
+
+               </BarChart>
+               </ResponsiveContainer>
+               
+            </Col>
+
+
+
+
                
                <Col lg={6} md={12} sm={12}>
-               <p className="text-justify serviceDescription">Hi! I'm Kazi Ariyan. I'm a web developer with a serious love for teaching I am a founder of eLe easy Learning and a passionate Web Developer, Programmer & Instructor.<br></br><br></br>
+     <p className="text-justify serviceDescription">Hi! I'm Kazi Ariyan. I'm a web developer with a serious love for teaching I am a founder of eLe easy Learning and a passionate Web Developer, Programmer & Instructor.<br></br><br></br>
 
 I am working online for the last 7 years and have created several successful websites running on the internet. I try to create a project-based course that helps you to learn professionally and make you fell as a complete developer. easy learning exists to help you succeed in life.<br></br><br></br>
 
