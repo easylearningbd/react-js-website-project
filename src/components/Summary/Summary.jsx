b014ba945bc39eca14c670f113294f6841290fb0
easylearningbd <elebdsoft@gmail.com>
2021-05-12 03:45:45 +0600

    26 Summary Component Part 4

--- src/components/Summary/Summary.jsx
+++ src/components/Summary/Summary.jsx
@@ -1,5 +1,10 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React, { Component, Fragment } from 'react'
 import { Card, Col, Container, Row } from 'react-bootstrap'
+import {faCheckSquare} from '@fortawesome/free-solid-svg-icons'
+import {faGlobe} from '@fortawesome/free-solid-svg-icons'
+import {faLaptop} from '@fortawesome/free-solid-svg-icons'
+import {faStar} from '@fortawesome/free-solid-svg-icons'
 
 class Summary extends Component {
      render() {
@@ -14,18 +19,21 @@ class Summary extends Component {
      <Col lg={8} md={6} sm={12}>
           <Row className="countSection">
                <Col>
+               <FontAwesomeIcon className="iconProject" icon={faGlobe} />
                <h1 className="countNumber">35000 </h1>
                <h4 className="countTitle">Students Worldwide</h4>
                <hr className="bg-white w-25" />
                </Col>
 
                <Col>
+               <FontAwesomeIcon className="iconProject" icon={faLaptop} />
                <h1 className="countNumber">22 </h1>
                <h4 className="countTitle">Courses Published</h4>
                <hr className="bg-white w-25" />
                </Col>
 
                <Col>
+               <FontAwesomeIcon className="iconProject" icon={faStar} />
                <h1 className="countNumber">3000 </h1>
                <h4 className="countTitle">Student Reviews</h4>
                <hr className="bg-white w-25" />
@@ -42,12 +50,12 @@ class Summary extends Component {
      <Card className="workCard" >
  
   <Card.Body>
-    <Card.Title className="cardTitle" >What I Have Achieved</Card.Title>
+    <Card.Title className="cardTitle">What I Have Achieved</Card.Title>
     <Card.Text>
-      <p className="cardSubTitle text-justify">Requirment Gathering </p>
-      <p className="cardSubTitle text-justify">System Analysis </p>
-      <p className="cardSubTitle text-justify">Coding Testing </p>
-      <p className="cardSubTitle text-justify">Implementation </p>
+      <p className="cardSubTitle text-justify"><FontAwesomeIcon className="iconBullent" icon={faCheckSquare} /> Requirment Gathering </p>
+      <p className="cardSubTitle text-justify"><FontAwesomeIcon className="iconBullent" icon={faCheckSquare} /> System Analysis </p>
+      <p className="cardSubTitle text-justify"><FontAwesomeIcon className="iconBullent" icon={faCheckSquare} /> Coding Testing </p>
+      <p className="cardSubTitle text-justify"><FontAwesomeIcon className="iconBullent" icon={faCheckSquare} /> Implementation </p>
     </Card.Text>
     
   </Card.Body>
