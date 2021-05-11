521990bf57cea018f090b95839eed89e73986177
easylearningbd <elebdsoft@gmail.com>
2021-05-12 05:05:38 +0600

    29 Project Component Part 2

--- src/components/RecentProject/RecentProject.jsx
+++ src/components/RecentProject/RecentProject.jsx
@@ -12,45 +12,45 @@ class RecentProject extends Component {
                         <Row>
                              <Col lg={4} md={6} sm={12}>
                               
-     <Card style={{ width: '18rem' }}>
+     <Card className="projectCard">
   <Card.Img variant="top" src="https://image.freepik.com/free-vector/online-courses-tutorials_52683-37860.jpg" />
   <Card.Body>
-    <Card.Title>Card Title</Card.Title>
-    <Card.Text>
+    <Card.Title className="serviceName">Project Name One</Card.Title>
+    <Card.Text className="serviceDescription">
       Some quick example text to build on the card title and make up the bulk of
       the card's content.
     </Card.Text>
-    <Button variant="primary">Go somewhere</Button>
+    <Button variant="primary">Vist WebSite</Button>
   </Card.Body>
 </Card>
                              </Col>
 
                              <Col lg={4} md={6} sm={12}>
 
-                             <Card style={{ width: '18rem' }}>
+     <Card className="projectCard">
   <Card.Img variant="top" src="https://image.freepik.com/free-vector/online-tutorials-concept_52683-37481.jpg" />
   <Card.Body>
-    <Card.Title>Card Title</Card.Title>
-    <Card.Text>
+    <Card.Title className="serviceName">Project Name Two</Card.Title>
+    <Card.Text className="serviceDescription">
       Some quick example text to build on the card title and make up the bulk of
       the card's content.
     </Card.Text>
-    <Button variant="primary">Go somewhere</Button>
+    <Button variant="primary">Vist WebSite</Button>
   </Card.Body>
 </Card>
                              </Col>
 
 
                              <Col lg={4} md={6} sm={12}>
-                            <Card style={{ width: '18rem' }}>
+     <Card className="projectCard">
   <Card.Img variant="top" src="https://image.freepik.com/free-vector/online-courses-concept_23-2148533386.jpg" />
   <Card.Body>
-    <Card.Title>Card Title</Card.Title>
-    <Card.Text>
+    <Card.Title className="serviceName">Project Name Three</Card.Title>
+    <Card.Text className="serviceDescription">
       Some quick example text to build on the card title and make up the bulk of
       the card's content.
     </Card.Text>
-    <Button variant="primary">Go somewhere</Button>
+    <Button variant="primary">Vist WebSite</Button>
   </Card.Body>
 </Card>
 
