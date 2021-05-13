5920942bfe5417c969708558812026b02bffd937
easylearningbd <elebdsoft@gmail.com>
2021-05-14 05:52:18 +0600

    40 FooterPart 2

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,8 @@ import { Col, Row, Container } from 'react-bootstrap'
 import {faFacebook} from '@fortawesome/free-brands-svg-icons'
 import {faYoutube} from '@fortawesome/free-brands-svg-icons'
 import {faTwitter} from '@fortawesome/free-brands-svg-icons'
-
+import {faEnvelope} from '@fortawesome/free-solid-svg-icons'
+import {faPhone} from '@fortawesome/free-solid-svg-icons'
 
 class Footer extends Component {
      render() {
@@ -12,34 +13,40 @@ class Footer extends Component {
                <Fragment>
       <Container fluid={true} className="footerSection">
                          <Row>
-     <Col lg="3" md={6} sm={12}>
+     <Col lg="3" md={6} sm={12} className="p-5 text-center">
      <h2 className="footerName text-center">Follow Us </h2>
           
-          <a href="#">
+          <div className="social-container">
+          <a className="facebook social" href="#">
              <FontAwesomeIcon icon={faFacebook} size="2x" />  
           </a>
-          <a href="#">
+          <a href="#"  className="youtube social">
           <FontAwesomeIcon icon={faYoutube} size="2x" />  
           </a>
-          <a href="#">
+          <a href="#"  className="twitter social">
           <FontAwesomeIcon icon={faTwitter} size="2x" />  
           </a> 
-
+          </div>
 
      </Col>
 
 
 
-     <Col lg="3" md={6} sm={12}>
+     <Col lg="3" md={6} sm={12} className="p-5 text-justify">
      <h2 className="footerName text-center">Address </h2>
+     <p className="footerDescription">
+          6522 Baltimore National Pike CatonsVille, Califonia USA <br></br>
+          <FontAwesomeIcon icon={faEnvelope}  /> Email : Support@ele.com<br></br>
+          <FontAwesomeIcon icon={faPhone}  /> Phone : 434343434<br></br>
+     </p>
      </Col>
 
 
-     <Col lg="3" md={6} sm={12}>
+     <Col lg="3" md={6} sm={12} className="p-5 text-justify">
      <h2 className="footerName text-center">Information </h2>
      </Col>
 
-     <Col lg="3" md={6} sm={12}>
+     <Col lg="3" md={6} sm={12} className="p-5 text-justify">
      <h2 className="footerName text-center">Policy  </h2>
      </Col>
                          </Row>
