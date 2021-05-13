a1cb5edb0a6b1c18a300f7bd7a751249308867a1
easylearningbd <elebdsoft@gmail.com>
2021-05-14 03:33:51 +0600

    36 Review SectionPart 2

--- src/components/ClientReview/ClientReview.jsx
+++ src/components/ClientReview/ClientReview.jsx
@@ -1,8 +1,57 @@
 import React, { Component, Fragment } from 'react'
 import { Col, Container, Row } from 'react-bootstrap'
+import "slick-carousel/slick/slick.css" 
+import "slick-carousel/slick/slick-theme.css"
+import Slider from "react-slick";
 
 class ClientReview extends Component {
      render() {
+
+          var settings = {
+               autoPlaySpeed:3000,
+               autoPlay:true,
+               dots: true,
+               infinite: true,
+               speed: 3000,
+               arrows:false,
+               vertical:true,
+               verticalSwiping:true,
+               slidesToShow: 1,
+               slidesToScroll: 1,
+               initialSlide: 1,
+               responsive: [
+                 {
+                   breakpoint: 1024,
+                   settings: {
+                     slidesToShow: 1,
+                     slidesToScroll: 1,
+                     infinite: true,
+                     dots: true
+                   }
+                 },
+                 {
+                   breakpoint: 600,
+                   settings: {
+                     slidesToShow: 1,
+                     slidesToScroll: 1,
+                     initialSlide: 1
+                   }
+                 },
+                 {
+                   breakpoint: 480,
+                   settings: {
+                     slidesToShow: 1,
+                     slidesToScroll: 1
+                   }
+                 }
+               ]
+             };
+
+
+
+
+
+
           return (
                <Fragment>
 
@@ -11,6 +60,8 @@ class ClientReview extends Component {
             <h1 className="reviewMainTitle p-3">TESTIMOIAL</h1>
                <div className="reviewbottom"></div> 
 
+          <Slider {...settings}>
+          <div>
           <Row className="text-center justify-content-center">
                <Col lg={6} md={6} sm={12}>
        <img className="circleImg" src="https://image.freepik.com/free-photo/handsome-young-man-with-new-stylish-haircut_176420-19637.jpg" />
@@ -19,7 +70,39 @@ class ClientReview extends Component {
 
                </Col>
           </Row>
+          </div>
 
+          <div>
+          <Row className="text-center justify-content-center">
+               <Col lg={6} md={6} sm={12}>
+       <img className="circleImg" src="https://image.freepik.com/free-photo/portrait-white-man-isolated_53876-40306.jpg" />
+       <h2 className="reviewName">Jack Ma</h2>
+      <p className="reviewDescription">Hi! I'm Kazi Ariyan. I'm a web developer with a serious love for teaching I am a founder of eLe easy Learning and a passionate Web Developer, Programmer & Instructor.</p>
+
+               </Col>
+          </Row>
+          </div>
+
+
+
+          <div>
+          <Row className="text-center justify-content-center">
+               <Col lg={6} md={6} sm={12}>
+       <img className="circleImg" src="https://image.freepik.com/free-photo/curly-man-with-broad-smile-shows-perfect-teeth-being-amused-by-interesting-talk-has-bushy-curly-dark-hair-stands-indoor-against-white-blank-wall_273609-17092.jpg" />
+       <h2 className="reviewName">Jhon </h2>
+      <p className="reviewDescription">Hi! I'm Kazi Ariyan. I'm a web developer with a serious love for teaching I am a founder of eLe easy Learning and a passionate Web Developer, Programmer & Instructor.</p>
+
+               </Col>
+          </Row>
+          </div>
+
+
+
+
+
+
+
+          </Slider>
 
 
 
