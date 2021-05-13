b78b9d78b462eafb64b7ce3cb0ffc8311316a6e0
easylearningbd <elebdsoft@gmail.com>
2021-05-14 02:18:45 +0600

    34 Video ComponentPart 3

--- src/components/Video/Video.jsx
+++ src/components/Video/Video.jsx
@@ -2,7 +2,8 @@ import React, { Component, Fragment } from 'react'
 import { Col, Container, Modal, Row,Button } from 'react-bootstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faVideoSlash} from '@fortawesome/free-solid-svg-icons'
-
+import 'video-react/dist/video-react.css'
+import { Player, BigPlayButton } from 'video-react'
 class Video extends Component {
 
      constructor(){
@@ -46,10 +47,14 @@ Each course has been hand-tailored to teach a specific skill. I hope you agree! 
 
                
          <Modal size="lg" show={this.state.show} onHide={this.modalClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        
+        <Modal.Body>
+
+        <Player src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4">
+      <BigPlayButton position="center" />
+    </Player>
+
+        </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={this.modalClose}>
             Close
