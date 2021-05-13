ab0bb0addba7c810145bc982cd1e9857c9be014c
easylearningbd <elebdsoft@gmail.com>
2021-05-14 04:46:35 +0600

    38 About Me SectionPart 2

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -1,7 +1,17 @@
 import React, { Component, Fragment } from 'react'
 import { Col, Container, Row } from 'react-bootstrap'
 import face from '../../asset/image/face.png';
+import { init } from 'ityped'
+
+
 class AboutMe extends Component {
+
+     componentDidMount(){
+          const myElement = document.querySelector('#myElement')
+          init(myElement, { showCursor: false, strings: ['Web Developer!', 'Online Instructor!' ] })
+        }
+
+
      render() {
           return (
                <Fragment>
@@ -22,6 +32,11 @@ class AboutMe extends Component {
 
 
      <Col lg={6} md={6} sm={12}>
+          <div className="aboutMeBody">
+               <h2 className="aboutMeDetails">HI There, I'm</h2>
+               <h2 className="aboutMeTitle">Kazi Ariyan</h2>
+               <h3 className="aboutMeDetails">Work as <span id="myElement"> </span> </h3>
+            </div>
      
      </Col>
 
