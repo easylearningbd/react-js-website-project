31bb03bf218ff85b7ef772dbd26435bc3db2fcc5
easylearningbd <elebdsoft@gmail.com>
2021-05-16 03:45:20 +0600

    49 Formation Service Page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ContactSec from './components/ContactSec/ContactSec';
 import AllCorusePage from './pages/AllCorusePage';
+import AllServicePage from './pages/AllServicePage';
 import ContactPage from './pages/ContactPage';
 import PortfolioPage from './pages/PortfolioPage';
  
@@ -10,7 +11,7 @@ function App() {
   return (
     <div>
     
-    <ContactPage />
+    <AllServicePage />
       
     </div>
   );
---
+++ src/pages/AllServicePage.jsx
@@ -0,0 +1,22 @@
+import React, { Component, Fragment } from 'react'
+import ContactSec from '../components/ContactSec/ContactSec'
+import Footer from '../components/Footer/Footer'
+import PageTop from '../components/PageTop/PageTop'
+import Services from '../components/Services/Services'
+import TopNavigation from '../components/TopNavigation/TopNavigation'
+
+class AllServicePage extends Component {
+     render() {
+          return (
+             <Fragment>
+                  <TopNavigation/>
+                  <PageTop pagetitle="Our Services" />
+                  <Services />
+                  <ContactSec />
+                  <Footer />
+             </Fragment>
+          )
+     }
+}
+
+export default AllServicePage
