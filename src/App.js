23d7b5cf4f212640208343ea3d50ccd2c667b92d
easylearningbd <elebdsoft@gmail.com>
2021-05-16 01:48:39 +0600

    43 Formation Home Page

--- src/App.js
+++ src/App.js
@@ -1,29 +1,10 @@
 import React from 'react';
-import TopBanner from './components/TopBanner/TopBanner';
-import Services from './components/Services/Services';
-import TopNavigation from './components/TopNavigation/TopNavigation';
-import Analysis from './components/Analysis/Analysis';
-import Summary from './components/Summary/Summary';
-import RecentProject from './components/RecentProject/RecentProject';
-import Courses from './components/Courses/Courses';
-import Video from './components/Video/Video';
-import ClientReview from './components/ClientReview/ClientReview';
-import AboutMe from './components/AboutMe/AboutMe';
-import Footer from './components/Footer/Footer';
+import HomePage from './pages/HomePage';
+
 function App() {
   return (
     <div>
-      <TopNavigation />
-      <TopBanner />
-      <Services />
-      <Analysis />
-      <Summary />
-      <RecentProject />
-      <Courses />
-      <Video />
-      <ClientReview />
-      <AboutMe />
-      <Footer />
+     <HomePage />
       
     </div>
   );
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,37 @@
+import React, { Component, Fragment } from 'react'
+import TopBanner from '../components/TopBanner/TopBanner';
+import Services from '../components/Services/Services';
+import TopNavigation from '../components/TopNavigation/TopNavigation';
+import Analysis from '../components/Analysis/Analysis';
+import Summary from '../components/Summary/Summary';
+import RecentProject from '../components/RecentProject/RecentProject';
+import Courses from '../components/Courses/Courses';
+import Video from '../components/Video/Video';
+import ClientReview from '../components/ClientReview/ClientReview';
+import AboutMe from '../components/AboutMe/AboutMe';
+import Footer from '../components/Footer/Footer';
+
+class HomePage extends Component {
+     render() {
+          return (
+             <Fragment>
+
+      <TopNavigation />
+      <TopBanner />
+      <Services />
+      <Analysis />
+      <Summary />
+      <RecentProject />
+      <Courses />
+      <Video />
+      <ClientReview />
+      <AboutMe />
+      <Footer />
+
+
+             </Fragment>
+          )
+     }
+}
+
+export default HomePage
