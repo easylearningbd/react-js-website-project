c31dcacdb62b8bd9ae1164481dc25e7b707a6637
easylearningbd <elebdsoft@gmail.com>
2021-05-16 04:52:06 +0600

    50 Formation Creating Route

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,29 @@
+import React, { Component, Fragment } from 'react'
+import {BrowserRouter as Router,Switch, Route, Link} from "react-router-dom";
+import AllCourses from '../components/AllCourses/AllCourses';
+import AboutPage from '../pages/AboutPage';
+import AllServicePage from '../pages/AllServicePage';
+import ContactPage from '../pages/ContactPage';
+import HomePage from '../pages/HomePage';
+import PortfolioPage from '../pages/PortfolioPage';
+
+class AppRouter extends Component {
+     render() {
+          return (
+               <Fragment>
+
+<Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/service" component={AllServicePage} />
+          <Route exact path="/course" component={AllCourses} />
+          <Route exact path="/porfolio" component={PortfolioPage} />
+          <Route exact path="/about" component={AboutPage} />
+          <Route exact path="/contact" component={ContactPage} /> 
+        </Switch>
+
+               </Fragment>
+          )
+     }
+}
+
+export default AppRouter
