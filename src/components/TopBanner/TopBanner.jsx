95b745e29915fba3f30a1a213e77d6a705b849cf
easylearningbd <elebdsoft@gmail.com>
2021-05-29 05:55:23 +0600

    114 Set Loading Animation Part 4

--- src/components/TopBanner/TopBanner.jsx
+++ src/components/TopBanner/TopBanner.jsx
@@ -3,20 +3,23 @@ import { Col, Container, Row, Button } from 'react-bootstrap'
 import axios from 'axios';
 import RestClient from '../../RestAPI/RestClient';
 import AppUrl from '../../RestAPI/AppUrl';
+import Loading from '../Loading/Loading';
 
 class TopBanner extends Component {
 
      constructor(){
           super();
           this.state={
-               title:".....",
-               subtitle:"....."
+               title:"",
+               subtitle:"",
+               loaderClass:"text-center",
+               mainDivClass:"d-none"
           }
      }
 
      componentDidMount(){
           RestClient.GetRequest(AppUrl.HomeTopTitle).then(result=>{
-               this.setState({title:result[0]['home_title'],subtitle:result[0]['home_subtitle']});
+               this.setState({title:result[0]['home_title'],subtitle:result[0]['home_subtitle'],loaderClass:"d-none",mainDivClass:"text-center"});
           }).catch(error=>{
               this.setState({title:"????",subtitle:"????"})
           });
@@ -32,7 +35,11 @@ class TopBanner extends Component {
                <div className="topBannerOverlay">
                <Container className="topContent">
                     <Row>
-               <Col className="text-center">
+                         <Col className={this.state.loaderClass}>
+                         <Loading/>                         
+                         </Col>
+
+               <Col className={this.state.mainDivClass}>
                <h1 className="topTitle">{this.state.title}</h1>
                <h4 className="topSubTitle">{this.state.subtitle}</h4>
                <Button variant="primary">Learn More</Button>
